dd49d5072d06fc63c386376821d9d372303d27e3
Aleftina17 <teekupinmuotti@gmail.com>
2023-12-05 11:12:43 +0300

    fix: making request

--- src/js/pages/making-request-page.js
+++ src/js/pages/making-request-page.js
@@ -1,33 +1,35 @@
 import $ from "jquery";
 
-$(function () {
-    $("#executorInputWrapper").on("click", function () {
-        $(".executor-list").slideToggle("fast");
-        $(".btn__open-list").toggleClass("open");
-    });
+// $(function () {
+//     $("#executorInputWrapper").on("click", function () {
+//         $(".executor-list").slideToggle("fast");
+//         $(".btn__open-list").toggleClass("open");
+//     });
 
-    $(document).on("click", function (e) {
-        if (!$(e.target).closest(".executor-list, #executorInputWrapper").length) {
-            $(".executor-list").slideUp("fast");
-        }
-    });
+//     $(document).on("click", function (e) {
+//         if (!$(e.target).closest(".executor-list, #executorInputWrapper").length) {
+//             $(".executor-list").slideUp("fast");
+//         }
+//     });
 
-    $("#executorInputWrapper input").on("input", function () {
-        var searchTerm = $(this).val().toLowerCase();
-        $(".executor-list__option").each(function () {
-            var text = $(this).find(".executor-list__title").text().toLowerCase();
-            if (text.includes(searchTerm)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    });
+//     $("#executorInputWrapper input").on("input", function () {
+//         let searchTerm = $(this).val().toLowerCase();
+//         $(".executor-list__option").each(function () {
+//             let text = $(this).find(".executor-list__title").text().toLowerCase();
+//             if (text.includes(searchTerm)) {
+//                 $(this).show();
+//             } else {
+//                 $(this).hide();
+//             }
+//         });
+//     });
 
-    $(".executor-list__option").on("click", function (e) {
-        e.stopPropagation();
-        var selectedText = $(this).find(".executor-list__title").text();
-        $("#executorInputWrapper input").val(selectedText);
-        $(".executor-list").slideUp("fast");
-    });
-});
+//     $(".executor-list__option").on("click", function (e) {
+//         e.stopPropagation();
+//         let selectedText = $(this).find(".executor-list__title").text();
+//         $("#executorInputWrapper input").val(selectedText);
+//         $(".executor-list").slideUp("fast");
+//     });
+// });
+
+
