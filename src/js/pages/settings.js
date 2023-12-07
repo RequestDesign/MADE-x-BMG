45bc4ada09be4332670c2474aab0708a52776e6a
MonstersBY <monsters.alex0@gmail.com>
2023-12-07 14:50:26 +0300

    feat: sms modal

--- src/js/pages/settings.js
+++ src/js/pages/settings.js
@@ -7,9 +7,6 @@ $('.settings__center_input_open').on('click', function(e){
         $(this).closest('.settings__center_input').find('input').attr("readonly", true); 
     }
 })
-$('.settings__right_hidden_btn').on('click', function(e){
-    $(this).closest('.settings__right_hidden').css('display', 'none')
-})
 $('.users__tenant_settings').on('click', function(e){
     $(this).closest('.users__tenant_item').find('.users__tenant_choice').slideToggle()
     $(this).closest('.users__tenant_item').toggleClass('active')
@@ -22,3 +19,8 @@ $('.users__tenant_checkbox input').on('change', function(e){
         $(this).closest('.users__tenant').find('.users__tenant_add_btn').prop("disabled", true)
     }
 })
+
+
+// $('.settings__right_hidden_btn').on('click', function(e){
+//     $(this).closest('.settings__right_hidden').css('display', 'none')
+// })
