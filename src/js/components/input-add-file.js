c45f4f31dd78ec2c8f05a3ac11bd526ad8613481
MonstersBY <monsters.alex0@gmail.com>
2024-01-19 11:06:36 +0300

    feat: fix

--- src/js/components/input-add-file.js
+++ src/js/components/input-add-file.js
@@ -26,7 +26,7 @@ $("#inputFile").on("change", function () {
                         e.target.result +
                         '" alt="Thumbnail" /><button class="input-file__thumbnail__btn-delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 21.25C17.1086 21.25 21.25 17.1086 21.25 12C21.25 6.89137 17.1086 2.75 12 2.75C6.89137 2.75 2.75 6.89137 2.75 12C2.75 17.1086 6.89137 21.25 12 21.25Z" fill="#101010"/><path d="M14.3936 9.59375L9.60156 14.3857" stroke="white" stroke-linecap="round"/><path d="M14.3976 14.3907L9.60156 9.59375" stroke="white" stroke-linecap="round"/></svg></button>'
                 );
-                input.value = "";
+                // input.value = "";
             };
 
             reader.readAsDataURL(file);
--- src/js/components/modal-success.js
+++ src/js/components/modal-success.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 $('[data-modal="share-pass-final"]').on("click", function () {
     $(".modal__share-pass").removeClass("active");
-    $(".modal__success").addClass("active");
+    // $(".modal__success").addClass("active");
 });
 
 $('[data-modal="move-choice"]').on("click", function () {
@@ -17,17 +17,17 @@ $('[data-modal="close-modal-ok"]').on("click", function () {
 
 $('[data-modal="move-choice-final"]').on("click", function (e) {
     e.preventDefault()
-    $(".modal__success").addClass("active");
+    // $(".modal__success").addClass("active");
 });
 
 $('[data-modal="success"]').on("click", function (e) {
     e.preventDefault()
-    $(".modal__success").addClass("active");
+    // $(".modal__success").addClass("active");
 });
 
 $('[data-modal="make-request"]').on("click", function (e) {
     e.preventDefault()
-    $(".modal__success").addClass("active");
+    // $(".modal__success").addClass("active");
 });
 
 $('[data-modal="add-open"]').on("click", function (e) {
@@ -38,5 +38,5 @@ $('[data-modal="add-open"]').on("click", function (e) {
 $('[data-modal="success-open"]').on("click", function (e) {
     e.preventDefault()
     $(".modal__move-choice").removeClass("active");
-    $(".modal__success").addClass("active");
+    // $(".modal__success").addClass("active");
 });
