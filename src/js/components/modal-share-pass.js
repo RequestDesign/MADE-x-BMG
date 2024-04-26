c15bef1b5d9cbc126bbd948d26ab0bd66d7d3b76
Aleftina17 <teekupinmuotti@gmail.com>
2024-04-26 14:04:37 +0300

    fix: modal users list

--- src/js/components/modal-share-pass.js
+++ src/js/components/modal-share-pass.js
@@ -3,203 +3,270 @@ import $ from "jquery";
 $('[data-modal="share-pass"]').on("click", function () {
     $(".modal__pass").removeClass("active");
     $(".modal__share-pass").addClass("active");
-    if($('.modal__share-pass__user-list').length){
-        uncheckAllUsers();
-    }
-    if($('.modal__share-pass__department-list').length){
-        uncheckAllDepartment();
-    }
-    updateShareButtonState();
+    // if($('.modal__share-pass__user-list').length){
+    //     uncheckAllUsers();
+    // }
+    // if($('.modal__share-pass__department-list').length){
+    //     uncheckAllDepartment();
+    // }
+    // updateShareButtonState();
 });
 $('[data-modal="share-pass-dep"]').on("click", function () {
     $(".modal__pass").removeClass("active");
     $(".modal__share-pass-dep").addClass("active");
-    if($('.modal__share-pass__user-list').length){
-        uncheckAllUsers();
-    }
-    if($('.modal__share-pass__department-list').length){
-        uncheckAllDepartment();
-    }
-    updateShareButtonState();
+    // if($('.modal__share-pass__user-list').length){
+    //     uncheckAllUsers();
+    // }
+    // if($('.modal__share-pass__department-list').length){
+    //     uncheckAllDepartment();
+    // }
+    // updateShareButtonState();
 });
 $('[data-modal="share-pass-final"]').on("click", function () {
     $(".modal__share-pass").removeClass("active");
-    uncheckAllUsers();
-    updateShareButtonState();
+    // uncheckAllUsers();
+    // updateShareButtonState();
 });
 $('[data-modal="move-choice"]').on("click", function () {
     $(".modal__share-pass").removeClass("active");
-    uncheckAllDepartment();
-    updateShareButtonState();
+    // uncheckAllDepartment();
+    // updateShareButtonState();
 });
 $('[data-modal="close-modal"]').on("click", function () {
     $(".modal__share-pass").removeClass("active");
-    uncheckAllUsers();
-    updateShareButtonState();
+    // uncheckAllUsers();
+    // updateShareButtonState();
 });
 
-const userListContainer = $(".modal__share-pass__user-list");
-const users = [
-    {
-        name: "Смирнов Евгений Сергеевич",
-        role: "арендатор",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-1.webp",
-        checked: false,
-    },
-    {
-        name: "Иванчук Лариса Викторовна",
-        role: "старший администратор",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-2.webp",
-        checked: false,
-    },
-    {
-        name: "Константинов Максим Петрович",
-        role: "кассир",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-1.webp",
-        checked: false,
-    },
-    {
-        name: "Иванчук Лариса Викторовна",
-        role: "продавец-консультант",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-2.webp",
-        checked: false,
-    },
-    {
-        name: "Иванчук Лариса Андреевна",
-        role: "уборщица",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-2.webp",
-        checked: false,
-    },
-    {
-        name: "Константинов Максим Петрович",
-        role: "арендатор",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-1.webp",
-        checked: false,
-    },
-    {
-        name: "Смирнов Евгений Андреевич",
-        role: "арендатор",
-        workplace: "Магазин Цветы",
-        photo: "./assets/images/common/user-1.webp",
-        checked: false,
-    },
-];
-const department = [
-    {
-        name: "Отдел Комерциализвации",
-        checked: false,
-    },
-    {
-        name: "Отдел Комерциализвации 2",
-        checked: false,
-    },
-    {
-        name: "Отдел Комерциализвации 3",
-        checked: false,
-    },
-    {
-        name: "Отдел Комерциализвации 4",
-        checked: false,
-    },
-    {
-        name: "Отдел Комерциализвации 5",
-        checked: false,
-    },
-    {
-        name: "Calzedonia",
-        checked: false,
-    },
-    {
-        name: "Calzedonia 1",
-        checked: false,
-    },
-];
+// const userListContainer = $(".modal__share-pass__user-list");
+
+// const users = [
+//     {
+//         name: "Смирнов Евгений Сергеевич",
+//         role: "арендатор",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-1.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Иванчук Лариса Викторовна",
+//         role: "старший администратор",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-2.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Константинов Максим Петрович",
+//         role: "кассир",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-1.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Иванчук Лариса Викторовна",
+//         role: "продавец-консультант",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-2.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Иванчук Лариса Андреевна",
+//         role: "уборщица",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-2.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Константинов Максим Петрович",
+//         role: "арендатор",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-1.webp",
+//         checked: false,
+//     },
+//     {
+//         name: "Смирнов Евгений Андреевич",
+//         role: "арендатор",
+//         workplace: "Магазин Цветы",
+//         photo: "./assets/images/common/user-1.webp",
+//         checked: false,
+//     },
+// ];
+
+// const department = [
+//     {
+//         name: "Отдел Комерциализвации",
+//         checked: false,
+//     },
+//     {
+//         name: "Отдел Комерциализвации 2",
+//         checked: false,
+//     },
+//     {
+//         name: "Отдел Комерциализвации 3",
+//         checked: false,
+//     },
+//     {
+//         name: "Отдел Комерциализвации 4",
+//         checked: false,
+//     },
+//     {
+//         name: "Отдел Комерциализвации 5",
+//         checked: false,
+//     },
+//     {
+//         name: "Calzedonia",
+//         checked: false,
+//     },
+//     {
+//         name: "Calzedonia 1",
+//         checked: false,
+//     },
+// ];
 
 //creating user list
-function renderUserList() {
-    // sort users to show checked users first
-    users.sort((a, b) => (b.checked ? 1 : 0) - (a.checked ? 1 : 0));
+// let users =[]
+// let department =[]
 
-    // render sorted users
-    userListContainer.html(
-        users
-            .map(
-                (user, index) => `
-            <li class="modal__share-pass__user ${user.checked ? 'checked' : ''}">
-                <div class="modal__share-pass__user__photo"><img src="${user.photo}" alt="User" /></div>
-                <div class="modal__share-pass__user__info">
-                    <div class="modal__share-pass__user__name">${user.name}</div>
-                    <div class="modal__share-pass__user__role">
-                        <span class="job">${user.role}</span> | <span class="workplace">${user.workplace}</span>
-                    </div>
-                </div>
-                <div class="modal__share-pass__user__checkbox checkbox-wrapper">
-                    <input type="checkbox" id="user${index}" ${user.checked ? "checked" : ""} data-index="${index}" />
-                    <label for="user${index}" class="checkbox-icon"></label>
-                </div>
-            </li>`
-            )
-            .join("")
-    );
-}
+// function renderUserList() {
+//     $('.modal__share-pass__user').each(function() {
+//         let $user = $(this);
+        
+//         let name = $user.find('.modal__share-pass__user__name').text().trim();
+//         let role = $user.find('.modal__share-pass__user__role .job').text().trim();
+//         let workplace = $user.find('.modal__share-pass__user__role .workplace').text().trim();
+//         let isChecked = $user.find('.modal__share-pass__user__checkbox input[type="checkbox"]').prop('checked');
+
+//         let user = {
+//             name: name,
+//             role: role,
+//             workplace: workplace,
+//             checked: isChecked
+//         };
+
+//         users.push(user);
+//     }); 
+    
+//     users.sort((a, b) => (b.checked ? 1 : 0) - (a.checked ? 1 : 0));
+//     // render sorted users
+//     // userListContainer.html(
+//     //     users
+//     //         .map(
+//     //             (user, index) => `
+//     //         <li class="modal__share-pass__user ${user.checked ? 'checked' : ''}">
+//     //             <div class="modal__share-pass__user__photo"><img src="${user.photo}" alt="User" /></div>
+//     //             <div class="modal__share-pass__user__info">
+//     //                 <div class="modal__share-pass__user__name">${user.name}</div>
+//     //                 <div class="modal__share-pass__user__role">
+//     //                     <span class="job">${user.role}</span> | <span class="workplace">${user.workplace}</span>
+//     //                 </div>
+//     //             </div>
+//     //             <div class="modal__share-pass__user__checkbox checkbox-wrapper">
+//     //                 <input type="checkbox" id="user${index}" ${user.checked ? "checked" : ""} data-index="${index}" />
+//     //                 <label for="user${index}" class="checkbox-icon"></label>
+//     //             </div>
+//     //         </li>`
+//     //         )
+//     //         .join("")
+//     // );
+// }
+
+// $(function() {
+//     if ($('.modal__share-pass').hasClass('active')) {
+//         renderUserList();
+//     }
+// });
 
 //creating department list
-function renderDepartmentList() {
-    // sort department to show checked department first
-    department.sort((a, b) => (b.checked ? 1 : 0) - (a.checked ? 1 : 0));
+// function renderDepartmentList() {
+//     $('.modal__share-pass__department').each(function(index) {
+//         let $department = $(this);
+        
+//         let name = $department.find('.modal__share-pass__user__name').text().trim();
+//         let isChecked = $department.find('.modal__share-pass__user__checkbox input[type="checkbox"]').prop('checked');
 
-    // render sorted department
-    $(".modal__share-pass__department-list").html(
-        department
-            .map(
-                (user, index) => `
-                <li class="modal__share-pass__department ${user.checked ? 'checked' : ''}">
-                    <div class="modal__share-pass__user__name">${user.name}</div>
+//         let departmentItem = {
+//             name: name,
+//             checked: isChecked
+//         };
 
-                    <div class="modal__share-pass__user__checkbox checkbox-wrapper">
-                        <input type="checkbox" id="user${index}" ${user.checked ? "checked" : ""} data-index="${index}" />
-                        <label for="user${index}" class="checkbox-icon"></label>
-                    </div>
-                </li>`
-            )
-            .join("")
-    );
-}
+//         department.push(departmentItem);
+//     }); 
+    
+//     department.sort((a, b) => (b.checked ? 1 : 0) - (a.checked ? 1 : 0));
 
-renderUserList();
-renderDepartmentList();
+//     // render sorted department
+//     // $(".modal__share-pass__department-list").html(
+//     //     department
+//     //         .map(
+//     //             (departmentItem, index) => `
+//     //             <li class="modal__share-pass__department ${departmentItem.checked ? 'checked' : ''}">
+//     //                 <div class="modal__share-pass__user__name">${departmentItem.name}</div>
+
+//     //                 <div class="modal__share-pass__user__checkbox checkbox-wrapper">
+//     //                     <input type="checkbox" id="department${index}" ${departmentItem.checked ? "checked" : ""} data-index="${index}" />
+//     //                     <label for="department${index}" class="checkbox-icon"></label>
+//     //                 </div>
+//     //             </li>`
+//     //         )
+//     //         .join("")
+//     // );
+// }
+
+// $(function() {
+//     if ($('.modal__share-pass__department').hasClass('active')) {
+//         renderUserList();
+//     }
+// });
+
+// renderUserList();
+// renderDepartmentList();
 
 // handle checkbox changes
-$(document).on("change", ".modal__share-pass__user__checkbox input", function () {
-    const index = $(this).data("index");
-    if($('.modal__share-pass__user-list').length){
-        users[index].checked = $(this).prop("checked");
-        renderUserList();
-    }
-    if($('.modal__share-pass__department-list').length){
-        department[index].checked = $(this).prop("checked");
-        renderDepartmentList();
-    }
-    // if($('.modal__share-pass__department-list').length){
-    //     department[index].checked = $(this).prop("checked");
-    //     renderDepartmentList();
-    // }
+// $(document).on("change", ".modal__share-pass__user__checkbox input", function () {
+//     const index = $(this).data("index");
+//     if($('.modal__share-pass__user-list').length){
+//         users[index].isChecked = $(this).prop("checked");
+//         renderUserList();
+//     }
+//     if($('.modal__share-pass__department-list').length){
+//         department[index].isChecked = $(this).prop("checked");
+//         renderDepartmentList();
+//     }
+//     // if($('.modal__share-pass__department-list').length){
+//     //     department[index].checked = $(this).prop("checked");
+//     //     renderDepartmentList();
+//     // }
 
-    updateShareButtonState();
-});
+//     updateShareButtonState();
+// });
+
+//uncheck all users when modal is closed
+// function uncheckAllUsers() {
+//     users.forEach((user) => {
+//         user.checked = false;
+//     });
+//     renderUserList();
+// }
+// function uncheckAllDepartment() {
+//     department.forEach((user) => {
+//         user.checked = false;
+//     });
+//     renderDepartmentList();
+// }
+
+//make btn active when at least one user was choosen
+// function updateShareButtonState() {
+//     $('[data-modal="share-pass-final"]').prop('disabled', !users.some((user) => user.isChecked));
+//     $('[data-modal="move-choice"]').prop('disabled', !department.some((user) => user.isChecked))
+//     if($('.users').length) {
+//         $('.btn__share-pass-final').prop('disabled', !users.some((user) => user.isChecked));
+//     }
+// }
 
 
-// handle search input
 $(function () {
     $(".modal__share-pass__search input").on("input", function () {
         const searchTerm = $(this).val().toLowerCase();
-
         if($('.modal__share-pass__user-list').length){
             $(".modal__share-pass__user-list .modal__share-pass__user").each(function () {
                 const userName = $(this).find(".modal__share-pass__user__name").text().toLowerCase();
@@ -214,9 +281,7 @@ $(function () {
         if($('.modal__share-pass__department-list').length){
             $(".modal__share-pass__department-list .modal__share-pass__department ").each(function () {
                 const userName = $(this).find(".modal__share-pass__user__name").text().toLowerCase();
-    
                 const isMatch = userName.includes(searchTerm);
-    
                 $(this).toggle(isMatch);
             });
         }
@@ -224,26 +289,13 @@ $(function () {
     });
 });
 
-//uncheck all users when modal is closed
-function uncheckAllUsers() {
-    users.forEach((user) => {
-        user.checked = false;
-    });
-    renderUserList();
-}
-function uncheckAllDepartment() {
-    department.forEach((user) => {
-        user.checked = false;
-    });
-    renderDepartmentList();
-}
-
-//make btn active when at least one user was choosen
-function updateShareButtonState() {
-    $('[data-modal="share-pass-final"]').prop('disabled', !users.some((user) => user.checked));
-    $('[data-modal="move-choice"]').prop('disabled', !department.some((user) => user.checked))
-    if($('.users').length) {
-        $('.btn__share-pass-final').prop('disabled', !users.some((user) => user.checked));
+$(function() {
+    function updateButtonState() {
+        let anyChecked = $('.modal__share-pass__user__checkbox input[type="checkbox"]:checked').length > 0;
+        $('.btn__share-pass-final').prop('disabled', !anyChecked);
     }
-}
-updateShareButtonState();
+    updateButtonState();
+    $(document).on('change', '.modal__share-pass__user__checkbox input[type="checkbox"]', function() {
+        updateButtonState();
+    });
+});
